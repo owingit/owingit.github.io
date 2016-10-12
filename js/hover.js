317d56eb300f785f5970fec5a59b8a9e0ac3f183
Owen Martin <martin.owen22@gmail.com>
2016-10-12 12:30:20 +0200

    align poem

---
+++ js/hover.js
@@ -0,0 +1,11 @@
+$('ul li img').hover(
+    function(){
+        $(this).css('opacity','.5');
+        var a = $(this).attr('alt');
+        $(this).parent().append('<div class="title">' + a + '</div>');
+    },
+    function(){
+        $(this).css('opacity','1');
+        $(this).next().remove('.title');
+    }
+);
