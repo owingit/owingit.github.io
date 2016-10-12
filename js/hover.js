e5454a0f1adee3c87fe820685cbf81b9a40c59e3
Owen Martin <martin.owen22@gmail.com>
2016-10-12 16:19:37 +0200

    words hovering

--- js/hover.js
+++ js/hover.js
@@ -4,9 +4,7 @@ $('img').hover(
         $(this).css('opacity','.5');
         var a = $(this).attr('alt');
         $(this).parent().append('<div class="title">' + a + '</div>');
-    },
-    function(){
-        $(this).css('opacity','1');
-        $(this).next().remove('.title');
+        // $(this).css('opacity','1');
+        // $(this).next().remove('.title');
     }
 );
